a70058a60859d46a9acd85dc4d165ec1f80600f1
Sanskargupta0 <sanskar362002@gmail.com>
2024-11-12 21:53:18 +0530

    Added: Get App Section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Hero from '../components/Hero';
 import Camp from '@/components/Camp';
 import Guide from '@/components/Guide';
 import Features from '@/components/Features';
+import GetApp from '@/components/GetApp';
 export default function Home() {
   return (
     <>
@@ -9,6 +10,7 @@ export default function Home() {
     <Camp />
     <Guide />
     <Features />
+    <GetApp />
     </>
   );
 }
